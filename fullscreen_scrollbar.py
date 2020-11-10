167c7d18f764625da0b5204c46528891b4967253
humanbeing-dev <maciej.sitowski@gmail.com>
2020-11-10 07:23:07 +0100

    Describe fullscreen_scrollbar application

--- fullscreen_scrollbar.py
+++ fullscreen_scrollbar.py
@@ -1,4 +1,15 @@
-"""Application that creates scrollbar along whole tkinter window"""
+"""Application that creates scrollbar along whole tkinter window
+
+Workflow:
+1. Create main frame
+2. Create canvas object
+3. Create scroll element
+4. Set scroll command to: canvas.yview (returns tuple of ytop, ybottom)
+5. Configure canvas yscrollcommand to: scroll.set (it jumps to given position of the canvas)
+6. Bind canvas <Configure> with lambda e: canvas.configure(scrollregion=canvas.bbox('all'))
+7. Create second frame on canvas
+8. Create window on canvas
+"""
 
 from tkinter import *
 from tkinter import ttk
@@ -22,27 +33,26 @@ class FullscreenScrollbar(Frame):
         canvas = Canvas(main_frame)
         canvas.pack(side=LEFT, fill=BOTH, expand=1)
 
-        #
+        # Create Scrollbar
         my_scroll = ttk.Scrollbar(main_frame, orient=VERTICAL, command=canvas.yview)
         my_scroll.pack(side=RIGHT, fill=Y)
-        # canvas.yview returns tuple with top and bottom
 
-
+        # Canvas configure and bind
         canvas.configure(yscrollcommand=my_scroll.set)
         canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox('all')))
+
+        # Create second frame
         second_frame = Frame(canvas)
         canvas.create_window((0, 0), window=second_frame, anchor='nw')
 
         # Create buttons
         for i in range(20):
             # Button(second_frame, width=12, text=f"Click {i}", command=lambda: print(canvas.yview())).grid(row=i, column=0)
-            Button(second_frame, width=12, text=f"Click {i}", command=lambda: my_scroll.set(0.0967741935483871, 0.7387096774193549)).grid(row=i, column=0)
-            # Button(second_frame, width=12, text=f"Click {i}", command=lambda: print(canvas.bbox('all'))).grid(row=i, column=0)
-
-
+            # Button(second_frame, width=12, text=f"Click {i}", command=lambda: my_scroll.set(0.0967741935483871, 0.7387096774193549)).grid(row=i, column=0)
+            Button(second_frame, width=12, text=f"Click {i}", command=lambda: print(canvas.bbox('all'))).grid(row=i, column=0)
 
 
 if __name__ == '__main__':
     root = Tk()
-    app = FullscreenScrollbar(master = root)
+    app = FullscreenScrollbar(master=root)
     app.mainloop()
