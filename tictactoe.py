06e16f14ddb325d69306e4d43bdb947892228db8
humanbeing-dev <maciej.sitowski@gmail.com>
2020-11-16 10:28:06 +0100

    Tictactoe - Add new_game function and checking if there is a tie

--- tictactoe.py
+++ tictactoe.py
@@ -8,16 +8,14 @@ class Application(Frame):
         self.master.title("Tic tac toe game")
 
         # Create game data
-        self.moves = iter(["O", "X"] * 5)
-        self.results = [["", "", ""], ["", "", ""], ["", "", ""]]
-        self.players = {"O": "Player 1", "X": "Player 2"}
+        self.load_variables()
 
         # Create header frame
         self.header_frame = Frame(master=self.master)
         self.header_frame.pack()
-        self.lbl = Label(master=self.header_frame, text="Game is on", font="Helvetica, 20")
-        self.lbl.grid(ipady=10, padx=50, row=0, column=0, columnspan=2)
-        self.reset_btn = Button(master=self.header_frame, text="New game", font="Helvetica, 10")
+        self.lbl = Label(master=self.header_frame, text="Game is on", font="Helvetica, 20", width=10)
+        self.lbl.grid(ipady=10, padx=40, row=0, column=0, columnspan=2)
+        self.reset_btn = Button(master=self.header_frame, text="New game", font="Helvetica, 10", command=self.new_game)
         self.reset_btn.grid(ipady=10, row=0, column=2)
 
         # Create game frame
@@ -34,17 +32,29 @@ class Application(Frame):
             btn.config(command=lambda r=row, c=column, b=btn: self.insert_move(r, c, b))
             btn.grid(row=row, column=column)
 
+    def load_variables(self):
+        self.moves = iter(["O", "X"] * 5)
+        self.results = [["", "", ""], ["", "", ""], ["", "", ""]]
+        self.players = {"O": "Player 1", "X": "Player 2"}
+        self.clicked_buttons = []
+        self.counter = 0
+
     def insert_move(self, row, column, btn):
         """Function that takes insert O or X on the board and in the result table"""
-        move = self.moves.__next__()
+        if btn not in self.clicked_buttons:
+            self.clicked_buttons.append(btn)
+            move = self.moves.__next__()
+            self.counter += 1
 
-        btn.config(text=move)
-        self.results[row][column] = move
+            btn.config(text=move)
+            self.results[row][column] = move
 
-        if self.check_if_end(row, column, move):
-            self.config_board_after_winning(move)
+            if self.check_if_there_is_winner(row, column, move):
+                self.config_board_after_winning(move)
+            elif self.counter == 9:
+                self.lbl.config(text=f"There is a tie")
 
-    def check_if_end(self, row, column, move):
+    def check_if_there_is_winner(self, row, column, move):
         """Function that checks all possible wins"""
         chk_row = all([self.results[row][i] == move for i in range(3)])
         chk_col = all([self.results[i][column] == move for i in range(3)])
@@ -59,7 +69,13 @@ class Application(Frame):
         self.lbl.config(text=f"{self.players[move]} won")
         [self.game_frame.children[button].config(state=DISABLED) for button in self.game_frame.children]
 
-    # TODO show button to reset game
+    def new_game(self):
+        """Reset variables and buttons"""
+        self.load_variables()
+        [self.game_frame.children[button].config(text=" ", state=NORMAL) for button in self.game_frame.children]
+        self.lbl.config(text=f"Game is on")
+
+    # TODO check if it is a tie
 
 
 if __name__ == "__main__":
